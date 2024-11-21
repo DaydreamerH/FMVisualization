06c6f4948df8d3500252c5f3dd13340f0eac361b
HPY <daydreamerpyh@gmail.com>
2024-11-21 16:23:57 +0800

    MainMenu组件化

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,75 @@
+<template>
+    <el-menu default-active="1" class="el-menu-vertical-demo" :unique-opened="true">
+        <el-sub-menu :index="String(index)" v-for="(menu, index) in menus" :key="menu.title">
+            <template #title>
+                <i :class="menu.icon"></i>
+                <span>{{ menu.title }}</span>
+            </template>
+            <el-menu-item-group>
+                <el-menu-item v-for="item in menu.children" :key="item.title" @click="handleMenuClick(item)">
+                    {{ item.title }}
+                </el-menu-item>
+            </el-menu-item-group>
+        </el-sub-menu>
+    </el-menu>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            menus: [
+                {
+                    title: "Burgers 方程",
+                    icon: "el-icon-s-grid",
+                    children: [
+                        { title: "折线图", action: "BL" },
+                        { title: "热力图", action: "BH" },
+                    ],
+                },
+                {
+                    title: "NS 方程",
+                    icon: "el-icon-s-data",
+                    children: [
+                        { title: "折线图", action: "NL" },
+                        { title: "热力图", action: "NH" },
+                        { title: "场图", action: "NF" },
+                    ],
+                },
+                {
+                    title: "M6 翼型数据",
+                    icon: "el-icon-s-operation",
+                    children: [{ title: "折线图", action: "ML" }],
+                },
+                {
+                    title: "NACA 翼型数据",
+                    icon: "el-icon-s-fold",
+                    children: [{ title: "折线图", action: "NAL" }],
+                },
+                {
+                    title: "CAST 翼型数据",
+                    icon: "el-icon-s-platform",
+                    children: [{ title: "折线图", action: "CL" }],
+                },
+                {
+                    title: "气动数据融合",
+                    icon: "el-icon-s-cooperation",
+                    children: [
+                        { title: "折线图", action: "FL" },
+                        { title: "3维图", action: "F3D" },
+                    ],
+                },
+            ],
+        }
+    },
+    methods: {
+        handleMenuClick(item) {
+            console.log(`Menu clicked: ${item.action}`);
+            // TODO: 根据 `action` 值切换到相应的功能或调用后端接口。
+        },
+    },
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import { createApp } from 'vue'
+import BurgersLiner from './components/parameters/BurgersLiner.vue'; 
+import MainMenu from './components/MainMenu.vue';        
 
-const app = createApp(App)
+const app = createApp(App);
 
+// 全局注册组件
+app.component('BurgersLiner', BurgersLiner);
+app.component('MainMenu', MainMenu);
 app.use(ElementPlus)
 app.mount('#app')
